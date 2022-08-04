e0bd8153be32a1739fbdfaefa4f7213fc3be8e82
Abdug'affor Abdurahimov <abdugaffor.abdurahimov@inoviagroup.se>
2022-08-04 10:02:24 +0500

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { makeColorStyle } from "./colors";
 import makeButton from "./button";
 
 const button = makeButton("Yay Press Me!");
+button.style = makeColorStyle("red")
 
 document.body.appendChild(button);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)();
+const { merge } = require("webpack-merge");
 
-module.exports = (env) => {
-  console.log({ env });
-  return {
-    mode: env.mode,
-    output: {
-      filename: "bundle.js",
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+  console.log("modeConfig", modeConfig(mode));
+  return merge(
+    {
+      mode,
+      output: {
+        filename: "bundle.js",
+      },
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
-    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
-  };
+    modeConfig(mode)
+  );
 };
